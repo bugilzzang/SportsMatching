f8adb1eb32509d68e595805bc1504f2b4aa35f7e
거부길 <sopens@naver.com>
2022-09-03 23:49:09 +0900

    Update SignDAO.java

---
+++ SportsMatching/src/main/java/SportsMatching/SignDAO.java
@@ -0,0 +1,32 @@
+package SportsMatching;
+
+import java.sql.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+
+public class SignDAO {
+	//test code
+	public String SignUP(String name, String id, String pw, String address, String mmr) {
+		String SQL = "insert into chat values(?,?,?,?,?)";
+		try (
+		    //DB 연결 
+			Connection conn = DBConnection.GetDB();
+			PreparedStatement ptstm = conn.prepareStatement(SQL);
+			){
+			ptstm.setString(1, name);
+			ptstm.setString(2, id);
+			ptstm.setString(3, pw);
+			ptstm.setString(4, address);
+			ptstm.setInt(5, Integer.parseInt(mmr));
+			
+		    ptstm.executeUpdate();
+						
+		    ptstm.close();
+			return "성공";
+		}catch (Exception e){
+			return "실패";
+		 }
+	 }
+}
