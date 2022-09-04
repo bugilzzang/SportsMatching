9419efd87226489e931014fdc47518a1b30d5d71
거부길 <sopens@naver.com>
2022-09-04 23:03:27 +0900

    Update SignDAO.java

--- SportsMatching/src/main/java/SportsMatching/SignDAO.java
+++ SportsMatching/src/main/java/SportsMatching/SignDAO.java
@@ -1,6 +1,8 @@
 package SportsMatching;
 
 import java.sql.*;
+import java.util.List;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
@@ -9,25 +11,42 @@ import javax.sql.DataSource;
 public class SignDAO {
 	//test code
 	public String SignUP(String name, String id, String pw, String address, String mmr) {
-		String SQL = "insert into user values(?,?,?,?,?)";
-		try (
+		String SQL1 = "select * from user where USER_ID = ?";
+		String SQL2 = "insert into user values(?,?,?,?,?)";
+		
+		try {
 		    //DB 연결 
 			Connection conn = DBConnection.GetDB();
-			PreparedStatement ptstm = conn.prepareStatement(SQL);
-			){
-			ptstm.setString(1, name);
-			ptstm.setString(2, id);
-			ptstm.setString(3, pw);
-			ptstm.setString(4, address);
-			ptstm.setInt(5, Integer.parseInt(mmr));
+			PreparedStatement ptstn = conn.prepareStatement(SQL1);
+			ptstn.setString(1, name);
+			ResultSet rs = ptstn.executeQuery();
 			
-		    ptstm.executeUpdate();
+			if(rs.next()) {
+				ptstn.close();
+				return "중복";
+			}
+			else {
+			    try {
+			    	ptstn.close();
+			    	PreparedStatement ptstm = conn.prepareStatement(SQL2);
 					
-		    
-			return "성공";
-		 }catch (Exception e){
+					ptstm.setString(1, name);
+					ptstm.setString(2, id);
+					ptstm.setString(3, pw);
+					ptstm.setString(4, address);
+					ptstm.setInt(5, Integer.parseInt(mmr));
+				    ptstm.executeUpdate();
+				    
+				    ptstm.close();
+					return "성공";
+			    }catch (Exception e){
+					System.out.println(e.getMessage());
+					return "실패";
+			     }		    
+	         }
+		 }catch(Exception e) {
 			System.out.println(e.getMessage());
 			return "실패";
-		 }
+		  }
 	 }
-}
+ }
